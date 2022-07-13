d51a20f3a953579946d59a79894e853288d19664
Mohammad Reza Safari <safary.mr@gmail.com>
2022-07-13 16:42:58 +0430

    fix v3 compatibility

--- libs/shared-models/src/customers.model.ts
+++ libs/shared-models/src/customers.model.ts
@@ -1,11 +1,13 @@
 interface ICustomer {
-  id: number;
-  guid: string;
-  customer: string;
-  asset_type: string;
-  serial_number: string;
-  service_contract: boolean;
-  warranty: boolean;
+  record: {
+    id: number;
+    guid: string;
+    customer: string;
+    asset_type: string;
+    serial_number: string;
+    service_contract: boolean;
+    warranty: boolean;
+  }[]
 }
 
 interface IFilters {
